d43abf8b7e81751e1214d46617c485afc6406f45
Satyadevi999 <76684905+Satyadevi999@users.noreply.github.com>
2021-03-28 15:06:29 +0530

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -4,7 +4,7 @@ function addUser()
     player2_name=document.getElementById("player2_name_input").value;
 
     localStorage.setItem("player1_name",player1_name);
-    localStorage.setItem("player1_name",player2_name);
+    localStorage.setItem("player2_name",player2_name);
 
     window.location="quiz_game_page.html";
 }
